46520a08b97c39f10afa7efd1dd25882e56314c0
Callan Stretton <strettcall@yahoo.com>
2019-10-11 11:27:17 +1300

    tidy up functions

--- app/production/index.js
+++ app/production/index.js
@@ -98,28 +98,27 @@ In conjunction with .ss-htmleditorfield-file.image styling in /app/scss/global/_
 */
 
 $(document).ready(function () {
-        
+    
     if ($('.ss-htmleditorfield-file.image').length > 0) {
-
         addClearFix();
-
-        // apply a clear: both to the trailing element if image is set by itself (with and without a caption)
-        function addClearFix() {
-            $('.ss-htmleditorfield-file.image').each(function () {
-
-                if ($(this).parent().hasClass('captionImage')) {
-                    if ($(this).parent().hasClass('leftAlone') || $(this).hasClass('center') || $(this).hasClass('rightAlone')) {
-                        $(this).parent().next().css('clear', 'both');
-                    }
-                } else {
-                    if ($(this).hasClass('leftAlone') || $(this).hasClass('center') || $(this).hasClass('rightAlone')) {
-                        $(this).parent().next().css('clear', 'both');
-                    }
-                }
-            }); 
-        }
     }
 });
+
+// apply a clear: both to the trailing element if image is set by itself (with and without a caption)
+function addClearFix() {
+    $('.ss-htmleditorfield-file.image').each(function () {
+
+        if ($(this).parent().hasClass('captionImage')) {
+            if ($(this).parent().hasClass('leftAlone') || $(this).hasClass('center') || $(this).hasClass('rightAlone')) {
+                $(this).parent().next().css('clear', 'both');
+            }
+        } else {
+            if ($(this).hasClass('leftAlone') || $(this).hasClass('center') || $(this).hasClass('rightAlone')) {
+                $(this).parent().next().css('clear', 'both');
+            }
+        }
+    }); 
+}
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
@@ -145,77 +144,76 @@ $(document).ready(function () {
         $(window).on('resize', function () {
             resizeEmbeds();
         });
-
-
-        // calculate the aspect ratio by grabbing the inner iframe's original width and height and set ratio as a data attribute
-        // if a custom overwritten width has been set use this otherwise use the original width, set width as a data attribute
-        function setDataAttributes() {
-            $('.ss-htmleditorfield-file.embed').each(function () {
-                $(this).css('max-width', 'auto'); // this is set in place to not overwrite the custom set width
-
-                var originalWidth = $(this).children('iframe').css('width');
-                var originalHeight = $(this).children('iframe').css('height');
-
-                var aspectRatio = parseInt(originalHeight) / parseInt(originalWidth);
-                $(this).attr({ 'data-ratio': aspectRatio });
-
-                var overWrittenWidth = $(this).css('width');
-
-                if (parseInt(overWrittenWidth)) {
-                    $(this).attr({ 'data-width': overWrittenWidth });
-                } else {
-                    $(this).attr({ 'data-width': originalWidth });
-                }
-            });
-        }
-
-        // get the width and calculate the height from the width and aspect ratio data attributes set in the previous function
-        // set the width and height of the embed, replace the inner iframe width and height with a full 100%
-        // apply a clear: both to the trailing element if video is set by itself
-        function setInitialSizing() {
-            $('.ss-htmleditorfield-file.embed').each(function () {
-                var trueAspectRatio = $(this).data('ratio');
-                var trueWidth = (parseInt($(this).data('width')));
-                var trueHeight = Math.floor(trueWidth * trueAspectRatio);
-
-                $(this).css('width', trueWidth + 'px');
-                $(this).css('height', trueHeight + 'px');
-                $(this).children('iframe').css('width', '100%');
-                $(this).children('iframe').css('height', '100%');
-
-                $(this).css('max-width', '100%');
-
-                if ($(this).hasClass('leftAlone') || $(this).hasClass('center') || $(this).hasClass('rightAlone')) {
-                    $(this).next().css('clear', 'both');
-                }
-            }); 
-        }
-
-        // grab the original width as seen in the data atrribute
-        // grab the current width
-        // grab the aspect ratio from the data attribute
-        // if the current is smaller than the original width, adjust the height by setting it based on the aspect ratio
-        // add/adjust padding to the bottom if a caption is present
-        function resizeEmbeds() {
-            $('.ss-htmleditorfield-file.embed').each(function () {
-
-                var attributeAspectRatio = $(this).data('ratio');
-                var currentWidth = (parseInt($(this).css('width')));
-                var attributeWidth = (parseInt($(this).data('width')));
-
-                if (currentWidth < attributeWidth) {
-                    var newHeight = Math.floor(currentWidth * attributeAspectRatio);
-                    $(this).css('height', newHeight + 'px');
-                }
-
-                if ($(this).find('.caption').length > 0) {
-                    var captionHeight = $(this).find('.caption').height();
-                    $(this).css('padding-bottom', captionHeight + 17 + 'px');
-                }
-            }); 
-        }
     }
 });
+
+// calculate the aspect ratio by grabbing the inner iframe's original width and height and set ratio as a data attribute
+// if a custom overwritten width has been set use this otherwise use the original width, set width as a data attribute
+function setDataAttributes() {
+    $('.ss-htmleditorfield-file.embed').each(function () {
+        $(this).css('max-width', 'auto'); // this is set in place to not overwrite the custom set width
+
+        var originalWidth = $(this).children('iframe').css('width');
+        var originalHeight = $(this).children('iframe').css('height');
+
+        var aspectRatio = parseInt(originalHeight) / parseInt(originalWidth);
+        $(this).attr({ 'data-ratio': aspectRatio });
+
+        var overWrittenWidth = $(this).css('width');
+
+        if (parseInt(overWrittenWidth)) {
+            $(this).attr({ 'data-width': overWrittenWidth });
+        } else {
+            $(this).attr({ 'data-width': originalWidth });
+        }
+    });
+}
+
+// get the width and calculate the height from the width and aspect ratio data attributes set in the previous function
+// set the width and height of the embed, replace the inner iframe width and height with a full 100%
+// apply a clear: both to the trailing element if video is set by itself
+function setInitialSizing() {
+    $('.ss-htmleditorfield-file.embed').each(function () {
+        var trueAspectRatio = $(this).data('ratio');
+        var trueWidth = (parseInt($(this).data('width')));
+        var trueHeight = Math.floor(trueWidth * trueAspectRatio);
+
+        $(this).css('width', trueWidth + 'px');
+        $(this).css('height', trueHeight + 'px');
+        $(this).children('iframe').css('width', '100%');
+        $(this).children('iframe').css('height', '100%');
+
+        $(this).css('max-width', '100%');
+
+        if ($(this).hasClass('leftAlone') || $(this).hasClass('center') || $(this).hasClass('rightAlone')) {
+            $(this).next().css('clear', 'both');
+        }
+    }); 
+}
+
+// grab the original width as seen in the data atrribute
+// grab the current width
+// grab the aspect ratio from the data attribute
+// if the current is smaller than the original width, adjust the height by setting it based on the aspect ratio
+// add/adjust padding to the bottom if a caption is present
+function resizeEmbeds() {
+    $('.ss-htmleditorfield-file.embed').each(function () {
+
+        var attributeAspectRatio = $(this).data('ratio');
+        var currentWidth = (parseInt($(this).css('width')));
+        var attributeWidth = (parseInt($(this).data('width')));
+
+        if (currentWidth < attributeWidth) {
+            var newHeight = Math.floor(currentWidth * attributeAspectRatio);
+            $(this).css('height', newHeight + 'px');
+        }
+
+        if ($(this).find('.caption').length > 0) {
+            var captionHeight = $(this).find('.caption').height();
+            $(this).css('padding-bottom', captionHeight + 17 + 'px');
+        }
+    }); 
+}
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
