3dbff32a848a16e7e021eae3f2a85ca01a0aedf8
jalamanderman <salamander_run@protonmail.com>
2020-11-10 11:02:02 +1300

    - major package.json dependency updates - major webpack.config.js config updates - remove font awesome

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,86 @@
-process.traceDeprecation = true;
-module.exports = (env, argv) => {
+// webpack.config.js
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-	var development_mode = (argv.mode === 'development');
+const devMode = process.env.NODE_ENV !== 'production';
+const output_dir = __dirname +"/app/production";
 
-	var webpack = require('webpack');
-	var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-	var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-	var OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-
-	var node_dir = __dirname + '/node_modules';
-	var output_dir = __dirname +"/app/production";
-
-	var config = {
-		mode: process.env.NODE_ENV,
-		context: __dirname,
-		entry: "./app/js/index.js",
-		devtool: (development_mode ? 'source-map' : false),
-		output: {
-			path: output_dir,
-			filename: (development_mode ? 'index.js' : 'index.min.js')
-		},
-		module: {
-			rules: [
-				{
-					test: require.resolve('jquery'),
-	        		exclude: [
-	        			/node_modules/
-	        		],
-					use: 'expose-loader?jQuery!expose?$'
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						MiniCssExtractPlugin.loader,
-						{
-							loader: 'css-loader',
-							options: {
-								url: false,
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						}
-					]
-				},
-				{
-					// load external resources (ie Google fonts)
-					test: /.(gif|jpg|png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-					use: {
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]?[hash]',
-							limit: 10000000
-						}
+module.exports = {
+	entry: "./app/js/index.js",
+	output: {
+		path: output_dir,
+		filename: (devMode ? 'index.js' : 'index.min.js')
+	},
+	devtool: (devMode ? 'source-map' : false),
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['@babel/preset-env']
 					}
 				}
-			]
-		},
-		plugins: [
-			new webpack.optimize.OccurrenceOrderPlugin(),
-			new webpack.ProvidePlugin({
-			    $: "jquery",
-			    jQuery: "jquery",
-			    "window.jQuery": "jquery"
-			}),
-			new MiniCssExtractPlugin({
-				path: output_dir,
-				filename: (development_mode ? 'index.css' : 'index.min.css'),
-				sourceMap: true
-			})
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					'postcss-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { url: false },
+					},
+					'resolve-url-loader',
+					'postcss-loader',
+					{
+						loader: "sass-loader",
+						options: {}
+					}
+				]
+			},
+			{
+				test: require.resolve('jquery'),
+				exclude: [
+					/node_modules/
+				],
+				use: 'expose-loader?jQuery!expose?$'
+			}
+		]
+	},
+	watchOptions: {
+		ignored: /node_modules/,
+		aggregateTimeout: 300,
+		poll: 500
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin(),
+			new OptimizeCSSAssetsPlugin()
 		],
-		optimization: {
-			minimize: !development_mode,
-			minimizer: [
-				new UglifyJsPlugin({
-					cache: true,
-					parallel: true,
-					sourceMap: true
-				}),
-				new OptimizeCSSAssetsPlugin({})
-			]
-		},
-	};
-
-	return config;
-}
+	},
+	plugins: [
+		new FixStyleOnlyEntriesPlugin(),
+		new MiniCssExtractPlugin({
+			path: output_dir,
+			filename: (devMode ? 'index.css' : 'index.min.css'),
+			sourceMap: true
+		}),
+		new CleanWebpackPlugin(),
+	]
+};
